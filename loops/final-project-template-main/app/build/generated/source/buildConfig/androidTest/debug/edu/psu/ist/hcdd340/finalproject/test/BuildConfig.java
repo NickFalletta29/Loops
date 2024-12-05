5a0c5edcb7cd6eae98f8f51ca9bf3392005cb7f4
NickFalletta <nickyny29@optonline.net>
2024-12-05 13:42:22 -0500

    Finished main screen

---
+++ loops/final-project-template-main/app/build/generated/source/buildConfig/androidTest/debug/edu/psu/ist/hcdd340/finalproject/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package edu.psu.ist.hcdd340.finalproject.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "edu.psu.ist.hcdd340.finalproject.test";
+  public static final String BUILD_TYPE = "debug";
+}
